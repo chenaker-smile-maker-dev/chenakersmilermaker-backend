6fa2ceeaa2701d26ae6b3fb1b03a42ea1993b7b3
mouayed-keziz <m_keziz@estin.dz>
2025-12-01 12:21:50 +0100

    -

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,9 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\ScrambleServiceProvider::class,
-    App\Providers\HealthCheckServiceProvider::class,
-    App\Providers\ObserversServiceProvider::class,
-    App\Providers\Filament\PanelTranslationServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\PanelTranslationServiceProvider::class,
+    App\Providers\HealthCheckServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\ObserversServiceProvider::class,
+    App\Providers\ScrambleServiceProvider::class,
 ];
